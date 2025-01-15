af953c6a69071f43d1f865bd763c0a1ce2538f16
dwezis <emax151004@gmail.com>
2025-01-15 23:35:05 +0300

    Реализована структура базы данных для тестов

---
+++ taking-tests-bot/app/database/models.py
@@ -0,0 +1,50 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy import BigInteger, String, Integer, ForeignKey, Table
+from typing import List
+
+engine = create_async_engine(url='sqlite+aiosqlite:///db.sqlite3')
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
+
+class Test(Base):
+    __tablename__ = 'tests'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    category: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    questions: Mapped[List["Question"]] = relationship("Question", back_populates="test", cascade="all, delete")
+
+class Question(Base):
+    __tablename__ = 'questions'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    question_text: Mapped[str] = mapped_column(String(255), nullable=False)
+    correct_option: Mapped[int] = mapped_column(Integer, nullable=False)
+    test_id: Mapped[int] = mapped_column(ForeignKey("tests.id"))
+
+    test: Mapped["Test"] = relationship("Test", back_populates="questions")
+
+class Option(Base):
+    __tablename__ = 'options'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    option_text: Mapped[str] = mapped_column(String(255), nullable=False)
+    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
+
+    question: Mapped["Question"] = relationship("Question", back_populates="options")
+
+Question.options: Mapped[List["Option"]] = relationship("Option", back_populates="question", cascade="all, delete")
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- taking-tests-bot/main.py
+++ taking-tests-bot/main.py
@@ -2,9 +2,11 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
+from app.database.models import async_main
 
 
 async def main():
+    await async_main()
     bot = Bot(token='7318550393:AAHULV68ELpKtBNaOAZX7GKfbIwq6HTIY7s')
     dp = Dispatcher()
     dp.include_router(router)
